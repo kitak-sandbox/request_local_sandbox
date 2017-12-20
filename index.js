9846a2209addc0aeea5be41ce2bc0394b8c03d0b
Keisuke KITA <kei.kita2501@gmail.com>
2017-12-21 00:28:05 +0900

    Fix

--- index.js
+++ index.js
@@ -2,26 +2,39 @@ const express = require('express');
 const uuidv1 = require('uuid/v1');
 require('zone.js');
 
-class Foo {
-    constructor() {
-        console.log(`[trxId=${Zone.current.get('trxId')}]: create Foo instance`)
+class TrxIdVerifier {
+    verify(trxId) {
+        if (trxId === Zone.current.get('trxId')) {
+            console.log(`[trxId=${trxId}] Accept`)
+        } else {
+            console.error(`[trxId=${trxId}] Reject ${Zone.current.get('trxId')}`)
+        }
     }
 }
 
 const app = express();
+const wait = (sec) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, sec);
+    });
+};
 
 app.use(function (req, res, next) {
+    const trxId = uuidv1();
+    req.trxId = trxId;
     Zone.current.fork({
         properties: {
-            trxId: uuidv1(),
+            trxId: trxId,
         },
     }).run(next);
 });
 
 app.get('/', (req, res) => {
-    console.log(Zone.current.get('trxId'));
-    new Foo();
-    res.send('Hello World!');
+    setTimeout(() => {
+        const verifier = new TrxIdVerifier();
+        verifier.verify(req.trxId)
+        res.send('Hello World!');
+    }, Math.random() * 200 + 100);
 });
 
 app.listen(3000, () => {
